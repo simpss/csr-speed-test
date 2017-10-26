a2aa282af793c982dc9cd97a9b12580e56b9cb91
siim <siim.gitclient@talvik.eu>
2017-10-26 12:55:40 +0300

    loop fixed.

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ start=`date`
 
 count=1000
 
-for i in {1..${count}}
+for ((i=0; i<=count; i++));
 do
     echo "signing CSR $i"
     openssl ca -batch -config openssl-ca.cnf -policy signing_policy -extensions signing_req -out servercert.pem -infiles servercert.csr
