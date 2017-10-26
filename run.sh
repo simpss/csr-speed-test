5eebdc1bf417cf4bd21b7e84cadabb765d325735
siim <siim.gitclient@talvik.eu>
2017-10-26 12:52:11 +0300

    `` around date

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-start=date
+start=`date`
 
-for i in {1..15000}
+for i in {1..1000}
 do
     echo "signing CSR $i"
     openssl ca -batch -config openssl-ca.cnf -policy signing_policy -extensions signing_req -out servercert.pem -infiles servercert.csr
 done
 
-end=date
+end=`date`
 
 echo "script started $start and ended $end"
