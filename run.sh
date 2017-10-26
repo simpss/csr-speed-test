784a7fb7b1165c4d1cc4d62483ad2d2959e9e62d
siim <siim.gitclient@talvik.eu>
2017-10-26 12:46:22 +0300

    time measurements added

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-for i in {1..10}
+start=`date +%s`
+
+for i in {1..15000}
 do
     echo "signing CSR $i"
     openssl ca -batch -config openssl-ca.cnf -policy signing_policy -extensions signing_req -out servercert.pem -infiles servercert.csr
 done
+
+end=`date +%s`
+runtime=$((end-start))
+
+echo "script finished in $runtime"
